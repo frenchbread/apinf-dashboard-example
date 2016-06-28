2fb0e404f48a7aec2b916004f96ce1978508e6c5
Damir Mustafin <frenchthebread@gmail.com>
2016-06-28 16:20:34 +0300

    Call sync method on startup

--- server/startup.js
+++ server/startup.js
@@ -1,10 +1,2 @@
-const params = {
-  index: 'api-umbrella-logs-v1-2016-06',
-  type: 'log',
-  size: 10,
-  query: {
-    match_all: {}
-  }
-};
 
-Meteor.call('syncElasticSearchData')
+// Meteor.call('syncElasticSearchData')
