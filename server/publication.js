6a5693c71be861661b1997280a1dc4360c604bee
Damir Mustafin <frenchthebread@gmail.com>
2016-06-29 15:34:08 +0300

    Filtering & & sorting

--- server/publication.js
+++ server/publication.js
@@ -1,15 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import moment from 'moment';
 
 Meteor.publish('Analytics', function () {
-  return Analytics.find({}, {
-    fields: {
-      'response_time': 1,
-      'response_status': 1,
-      'request_at': 1,
-      'request_ip': 1,
-      'request_path': 1,
-      'response_status': 1
-    },
-    limit: 50000
-  });
+
+  // const start = moment().subtract(1, 'day').valueOf();
+
+  const logs = Analytics.find({
+    // request_at: { $gte: start }
+  }, {
+      limit: 10000,
+      sort: {
+        request_at: -1
+      }
+    }
+  );
+  return logs;
 })
