ff95985d0e9b3db3ab40a59901bb3e2f7516b187
Damir Mustafin <frenchthebread@gmail.com>
2016-08-04 14:59:02 +0300

    Chart

--- client/dashboard.js
+++ client/dashboard.js
@@ -4,6 +4,9 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import _ from 'lodash';
 import config from '/config';
 
+import d3 from 'd3';
+// import nvd3 from 'nvd3';
+
 Template.dashboard.onCreated(function(){
 
   const instance = this;
@@ -11,8 +14,9 @@ Template.dashboard.onCreated(function(){
   instance.cols = new ReactiveVar();
   instance.rows = new ReactiveVar();
 
-  instance.getAnalyticsDrilldown = async function () {
+  instance.dataSet = new ReactiveVar();
 
+  instance.getAnalyticsDrilldown = async function () {
 
     return await new Promise((resolve, reject) => {
       Meteor.call('getAnalyticsDrilldown', (err, res) => {
@@ -29,6 +33,7 @@ Template.dashboard.onCreated(function(){
       _.forEach(data.hits_over_time.cols, (col) => {
         cols.push(col.label);
       });
+
       let rows = [];
       _.forEach(data.hits_over_time.rows, (row) => {
         let cc = [];
@@ -37,13 +42,78 @@ Template.dashboard.onCreated(function(){
         });
         rows.push(cc);
       });
-      console.log(cols, rows);
+
+      console.log(data);
+
+      let dataSet = [];
+      for (let i = 1; i < data.hits_over_time.cols.length; i++) {
+
+        let obj = {
+          "key": '',
+          "values": []
+        };
+
+        obj.key = data.hits_over_time.cols[i].label;
+
+        for (let day = 0; day < data.hits_over_time.rows.length; day++) {
+
+          obj.values.push([data.hits_over_time.rows[day].c[0].f, data.hits_over_time.rows[day].c[i].v]);
+        }
+        dataSet.push(obj)
+      }
+
+      console.log(dataSet);
+      instance.dataSet.set(dataSet);
+
       instance.cols.set(cols);
       instance.rows.set(rows);
     })
     .catch((err) => {
       console.error(err);
     })
+});
+
+Template.dashboard.onRendered(function(){
+  const instance = this;
+
+  instance.autorun(() => {
+
+    const data = instance.dataSet.get();
+
+    if (data) {
+
+      nv.addGraph(function() {
+
+        var chart = nv.models.lineChart()
+        .options({
+          duration: 300,
+          useInteractiveGuideline: true
+        });
+        // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the parent chart, so need to chain separately
+        chart.xAxis
+        .axisLabel("Time (s)")
+        .tickFormat(d3.format(',.1f'))
+        .staggerLabels(true);
+
+        chart.yAxis
+        .axisLabel('Voltage (v)')
+        .tickFormat(function(d) {
+          if (d == null) {
+            return 'N/A';
+          }
+          return d3.format(',.2f')(d);
+        });
+
+        d3.select('#chart')
+        .datum(data)
+        .call(chart);
+
+        nv.utils.windowResize(chart.update);
+        
+        return chart;
+      });
+    }
+  });
 });
 
 Template.dashboard.helpers({
