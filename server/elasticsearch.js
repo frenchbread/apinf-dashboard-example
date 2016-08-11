6b30971fc239c72dba9d9ce074be1cea382e2619
Damir Mustafin <frenchthebread@gmail.com>
2016-08-11 13:40:36 +0300

    Cleanup

--- server/elasticsearch.js
+++
@@ -1,7 +0,0 @@
-import ElasticSearch from 'elasticsearch';
-
-import config from '/config';
-
-const host = config.host;
-
-export const esClient = new ElasticSearch.Client({ host });
--- server/methods.js
+++ server/methods.js
@@ -2,21 +2,10 @@ import { Meteor } from 'meteor/meteor';
 import { esClient } from '/server/elasticsearch';
 import config from '/config';
 
-Meteor.methods({
-  getElasticSearchData (opts) {
-
-    const start = new Date().getTime();
-
-    return esClient.search(opts).then((res) => {
-
-      console.log((new Date().getTime() - start) / 1000 );
-
-      return res;
-    });
-  },
+Meteor.methods(
   async getAnalyticsDrilldown () {
 
-    const url = `${config.apiUmbrella}/v1/analytics/drilldown?interval=day&start_at=2016-01-01&end_at=2016-08-03&prefix=0%2F`;
+    const url = `${config.host}/v1/analytics/drilldown?interval=day&start_at=2016-01-01&end_at=2016-08-03&prefix=0%2F`;
 
     return await new Promise((resolve, reject) => {
 
